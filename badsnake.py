447504b5e37e5ab6804cba701c198082e22c553a
Markus Rafeiner <mrafeiner@student.tgm.ac.at>
2022-10-01 13:44:26 +0200

    testing the LED-Matrix

--- badsnake.py
+++ badsnake.py
@@ -1,4 +1,5 @@
 from random import randint
+from sense_hat import SenseHat
 
 
 class Array:
@@ -35,6 +36,25 @@ class Array:
 
 
 if __name__ == '__main__':
+    H = [0, 0, 255]  # Head: blue
+    A = [255, 0, 0]  # Apple: red
+    B = [0, 255, 0]  # Body: green
+    F = [0, 0, 0]  # Free: white
+    s = SenseHat()
+    array = [
+        F, F, F, F, F, F, F, F,
+        F, F, F, F, F, F, F, F,
+        F, B, B, H, F, A, F, F,
+        F, B, F, F, F, F, F, F,
+        F, F, F, F, F, F, F, F,
+        F, F, F, F, F, F, F, F,
+        F, F, F, F, F, F, F, F,
+        F, F, F, F, F, F, F, F
+    ]
+    s.set_pixels(array)
+"""
+if __name__ == '__main__':
     print(Array.get_random())
     print(Array.convert_to_array(62))
     print(Array.convert_to_list(0, 7))
+"""
